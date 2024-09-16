2186a8a12795c695a5521e6be282b801596a5892
Prachi Jagtap <prachijagtap@Prachis-MacBook-Pro.local>
2024-09-16 17:57:38 +0530

    monorepo changes

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { ProductsComponent } from '@angular-monorepo/products';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ProductsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-store';
+  title = 'inventory';
 }
